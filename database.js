dfecff529277019c4fc5ecb3d3b783a9a3415d21
KgySathsara <88894196+KgySathsara@users.noreply.github.com>
2024-03-05 13:50:32 +0530

    first commit

---
+++ database.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+	host : 'localhost',
+	database : 'Node_testing',
+	user : 'root',
+	password : ''
+});
+
+connection.connect(function(error){
+	if(error)
+	{
+		throw error;
+	}
+	else
+	{
+		console.log('MySQL Database is connected Successfully');
+	}
+});
+
+module.exports = connection;
